6be4f3abae6378d74337cf402daa3080fcc812b6
Jeb Stone <jeb.stone@gmail.com>
2017-12-28 23:12:07 -0600

    Custom PostgreSQL / Python function to count blank values
    Custom PostgreSQL / Python function to count NULL, '', or ' ' values for all columns in a user-specified table. Helpful for speeding up certain EDA tasks.
    Expects: a table name, e.g., select * from count_missing('my_tablename')
    Returns: One record per column in the specified table; contains the column name, datatype, record count, missing count, and missing percent for each column.

---
+++ PostgreSQL_Python_function_count_missing.sql
@@ -0,0 +1,36 @@
+﻿CREATE PROCEDURAL LANGUAGE 'plpythonu' HANDLER plpython_call_handler; 
+
+CREATE OR REPLACE FUNCTION count_missing (table_name text)
+RETURNS TABLE (column_name text, column_type text, record_cnt integer, missing_cnt integer, missing_pct float)
+AS $$
+
+null_or_blank = ['character', 'character varying', 'json', 'text']
+
+sql = "SELECT attname AS col, atttypid::regtype AS datatype FROM pg_attribute WHERE attrelid = 'public.{}'::regclass AND attnum > 0 AND NOT attisdropped ORDER BY attnum;".format(table_name)
+column_info = plpy.execute(sql)
+column_cnt = column_info.nrows()
+
+results_table = []
+
+for i in range(0, column_cnt):
+  sql = ""
+  column_name = column_info[i]["col"]
+  column_type = column_info[i]["datatype"]
+
+  if ( column_type in null_or_blank ):
+    sql = "SELECT count(*) AS record_cnt, sum(case when {1} is null or trim(both ' ' from {1})  = '' then 1 else 0 end) AS missing_cnt, sum(case when {1} is null or trim(both ' ' from {1}) = '' then 1 else 0 end) * 1.0 / count(*) AS missing_pct from {0};".format(table_name, column_name)
+
+  else:
+    sql = "SELECT count(*) AS record_cnt, sum(case when {1} is null then 1 else 0 end) AS missing_cnt, sum(case when {1} is null then 1 else 0 end) * 1.0 / count(*) AS missing_pct from {0};".format(table_name, column_name)
+
+  column_stats = plpy.execute(sql)
+  record_cnt = column_stats[0]["record_cnt"]
+  missing_cnt = column_stats[0]["missing_cnt"]
+  missing_pct = round(column_stats[0]["missing_pct"], 3)
+
+  results_table.append([column_name, column_type, record_cnt, missing_cnt, missing_pct])
+  
+return results_table
+$$ LANGUAGE plpythonu;
+
+select * from count_missing('my_tablename')
